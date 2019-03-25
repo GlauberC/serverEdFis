1166a59db7d6254dcfe20bfff8b414e511a14300
GlauberC <glaubercarv@gmail.com>
2019-03-25 19:37:16 -0300

    add banco de dados do atlas

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV == 'production'){
-    module.exports = {mongoURI: ""}
+    module.exports = {mongoURI: "mongodb+srv://Glauber:Admin123321@siteedfis-le69g.mongodb.net/test?retryWrites=true"}
     
 }else{
     module.exports = {mongoURI: "mongodb://localhost/edfisdb"}
